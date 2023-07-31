653e37c62caadcebff2bb99076b2e1d37315ecfb
M0h4meD1k <140860767+M0h4meD1k@users.noreply.github.com>
2023-07-31 23:38:42 +0200

    Delete assets directory

--- assets/footer.php
+++
@@ -1,8 +0,0 @@
-<footer>
-    <div class="footer">
-        <div class="footer-text">
-                <p>&copy; Lobbycraft 2023&shy;</p>
-                <p>Created by M0h4me</p>
-        </div>
-    </div>
-</footer>
--- assets/main.php
+++
@@ -1,5 +0,0 @@
-<main>
-    <div class="subheader">
-        <img src="/img/obr3.png" alt="img" class="img">
-    </div>
-</main>
